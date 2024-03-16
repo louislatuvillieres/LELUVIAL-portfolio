339742bd8e943c2a67912fd109d1b9446b729472
Louis Latu-Villières <louis.latuvillieres@gmail.com>
2024-03-16 12:11:46 +0100

    feat(projects)

--- src/app/components/ProjectList.tsx
+++ src/app/components/ProjectList.tsx
@@ -23,7 +23,11 @@ const ProjectList: React.FC<ProjectListProps> = ({ projects }) => {
       {projects.map((project, index) => (
         <Link href={'/projets/'+project.slug} key={index} className=''>
             <div className='flex flex-col mb-8'>
-              <img src='https://dummyimage.com/600x400/cfcfcf/fff' className='w-full'/>
+              <div>
+                <img className='absolute w-20 -rotate-45 -translate-x-1/3' src={index % 2 === 0 ?'/bar_2.png' : '/bar_1.png'}/>
+                <img src='https://dummyimage.com/600x400/cfcfcf/fff' className='w-full'/>
+                <img className='absolute w-20 -rotate-45 right-0 -translate-y-full' src={index % 2 === 0 ?'/bar_1.png' : '/bar_2.png'}/>
+              </div>
               <div className='mx-1 mt-4'>
                 <div className='font-erode font-light text-3xl italic'>0{index+1}</div>
                 <div className='font-plex font-medium text-2xl -mt-2 '>{project.name}</div>
@@ -35,6 +39,7 @@ const ProjectList: React.FC<ProjectListProps> = ({ projects }) => {
                   </div>
                 ))}
               </div>
+              <span className='font-plex mx-1 text-[#87bbc7] text-right'>Voir plus...</span>
             </div>
         </Link>
       ))}
